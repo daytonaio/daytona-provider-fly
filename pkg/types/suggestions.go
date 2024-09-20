1b996ca549a48fe0437595bd18d1daf932420173
bcmmbaga <bethuelmbaga12@gmail.com>
2024-09-20 13:16:26 +0300

    chore: add regions suggestions
    Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>

---
+++ pkg/types/suggestions.go
@@ -0,0 +1,5 @@
+package types
+
+var (
+	regions = []string{"ams", "arn", "atl", "bog", "bom", "bos", "cdg", "den", "dfw", "ewr", "eze", "fra", "gdl", "gig", "gru", "hkg", "iad", "jnb", "lax", "lhr", "mad", "mia", "nrt", "ord", "otp", "phx", "qro", "scl", "sea", "sin", "sjc", "syd", "waw", "yul", "yyz"}
+)
